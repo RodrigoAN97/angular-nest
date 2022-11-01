1c3d97904bb1bf517decf5564637d739aeb6a947
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-11-01 20:18:55 -0300

    dummy module

---
+++ src/todo/todo.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get } from '@nestjs/common';
+import { TodoService } from './todo.service';
+
+@Controller('api/todos')
+export class TodoController {
+  constructor(private todoService: TodoService) {}
+
+  @Get()
+  findTodos(): string[] {
+    return ['hello', 'world'];
+  }
+}
