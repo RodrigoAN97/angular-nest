e8f72ae5548ce67ce2b71067bbb14f01c4122c50
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-11-02 17:12:10 -0300

    validate registration

--- src/Dtos/createUser.dto.ts
+++ src/Dtos/createUser.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsEmail, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
   readonly displayName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
   readonly password: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateUserDto } from 'src/Dtos/createUser.dto';
 import { UserService } from './user.service';
 import { IUserResponse } from './user.types';
@@ -8,6 +14,7 @@ export class UserController {
   constructor(private authService: UserService) {}
 
   @Post()
+  @UsePipes(new ValidationPipe())
   createUser(
     @Body('user') createUserDto: CreateUserDto,
   ): Promise<IUserResponse> {
