bfcfec435f4aff7dc6d981b2a346df0b844a29bd
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-11-03 17:27:05 -0300

    enable cors

--- todo-api/src/main.ts
+++ todo-api/src/main.ts
@@ -5,6 +5,7 @@ import { AuthGuard } from './guards/auth.guard';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalGuards(new AuthGuard(new Reflector()));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
