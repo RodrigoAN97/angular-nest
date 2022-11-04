e56032bc6b49050c2783356341a8995e9195fc26
RodrigoAN97 <rodrigo_neves97@outlook.com>
2022-11-04 10:05:59 -0300

    tooltip

--- todo-client/src/app/todo/todo.module.ts
+++ todo-client/src/app/todo/todo.module.ts
@@ -6,6 +6,7 @@ import { MatExpansionModule } from '@angular/material/expansion';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 export const childRoutes: Routes = [
   {
@@ -22,7 +23,8 @@ export const childRoutes: Routes = [
     MatExpansionModule,
     MatIconModule,
     MatButtonModule,
-    MatInputModule
+    MatInputModule,
+    MatTooltipModule
   ],
 })
 export class TodoModule {}
